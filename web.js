d24b9d4dd16ea8c9ad5749899841f8edbe5a8e39
Abdalah El-Barrad <ae738513@ohio.edu>
2017-06-11 13:42:05 -0400

    Copy Stuff

--- web.js
+++ web.js
@@ -4,6 +4,9 @@ var app = express();
 
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
+}).configure(function() {
+    app.use('/assets', express.static(__dirname + '/assets'));
+    app.use('/bower_components', express.static(__dirname + '/bower_components'));
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
