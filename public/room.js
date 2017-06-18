27a13b579c0fe3cbf6906dbd8abf705c8d39d37b
Abdalah El-Barrad <ae738513@ohio.edu>
2017-06-18 16:54:03 -0400

    Refactoring

--- public/room.js
+++ public/room.js
@@ -23,21 +23,24 @@ function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    colton = game.add.sprite(200, 200, 'colton');
+    game.physics.arcade.enable(colton, Phaser.Physics.ARCADE);
+
     floor = game.add.graphics(0, 0);
-    floor.beginFill(0x55a558);
-    floor.drawRect(50, 50, 815, 515);
+    floor.beginFill(0xdac292);
+    floor.drawRect(colton.width, colton.width, game.width-colton.width*2, game.height-colton.width*2);
     floor.endFill();
 
     walls = game.add.graphics(0, 0);
-    walls.beginFill(0xa58855);
-    walls.drawRect(50, game.world.height - 50, 815, 15);
-    walls.drawRect(50, 50, 815, 15);
-    walls.drawRect(game.world.width - 50, 50, 15, 500);
-    walls.drawRect(50, 50, 15, 500);
+    walls.beginFill(0xb9936c);
+    walls.drawRect(colton.width, game.world.height - colton.width - 15, game.width-colton.width*2, 15);
+    walls.drawRect(colton.width, colton.width, game.world.width - colton.width*2, 15);
+    walls.drawRect(game.world.width - colton.width-15, colton.width, 15, game.height-colton.width*2);
+    walls.drawRect(colton.width, colton.width, 15, game.height-colton.width*2);
     walls.endFill();
 
 
-    comp = game.add.sprite(game.world.width/2, 65, 'comp');
+    comp = game.add.sprite(game.world.width/2, colton.width+15, 'comp');
     comp.scale.setTo(.25, .25);
     comp.y -= comp.height;
     game.physics.arcade.enable(comp, Phaser.Physics.ARCADE);
@@ -53,6 +56,7 @@ function create() {
     music.play();
 
     game.input.addPointer();
+    game.input.addPointer();
 
 }
 
@@ -62,36 +66,75 @@ function update() {
     colton.body.velocity.x = 0;
     colton.body.velocity.y = 0;
 
-    if(colton.y < 67 && colton.x < game.width/2+comp.width+1 && colton.x+colton.width > game.width/2-1){
+    if(colton.y < colton.width+16 && colton.x < game.width/2+comp.width+1 && colton.x+colton.width > game.width/2-1){
         music.resume();
     }
     else{
         music.pause();
     }
 
-    if ((cursors.left.isDown || game.input.pointer1.x < game.width/3 & game.input.pointer1.active) && colton.x > 65)
-    {
-        //  Move to the left
-        colton.body.velocity.x = -150;
-        //music.pause();
+    if(game.input.pointer1.active && !game.input.pointer2.active){
+        if (game.input.pointer1.x < game.width/3 && colton.x > colton.width-15)
+        {
+            colton.body.velocity.x = -150;
+        }
+        else if (game.input.pointer1.x > 2*game.width/3 && colton.x+colton.width < game.width-colton.width+15)
+        {
+            //  Move to the left
+            colton.body.velocity.x = 150;
+        }
+        if (game.input.pointer1.y < game.height/3 && colton.y > colton.width+15)
+        {
+            //  Move to the left
+            colton.body.velocity.y = -150;
+        }
+        else if (game.input.pointer1.y > 2*game.height/3 && colton.y+colton.height < game.height-colton.width+15)
+        {
+            //  Move to the left
+            colton.body.velocity.y = 150;
+        }
     }
-    else if ((cursors.right.isDown || game.input.pointer1.x > 2*game.width/3 & game.input.pointer1.active) && colton.x+colton.width < game.width-50)
-    {
-        //  Move to the left
-        colton.body.velocity.x = 150;
-        //music.pause();
+    else if(!game.input.pointer1.active && game.input.pointer2.active){
+        if (game.input.pointer2.x < game.width/3 && colton.x > colton.width+15)
+        {
+            colton.body.velocity.x = -150;
+        }
+        else if (game.input.pointer2.x > 2*game.width/3 && colton.x+colton.width < game.width-colton.width-15)
+        {
+            //  Move to the left
+            colton.body.velocity.x = 150;
+        }
+        if (game.input.pointer2.y < game.height/3 && colton.y > colton.width+15)
+        {
+            //  Move to the left
+            colton.body.velocity.y = -150;
+        }
+        else if (game.input.pointer2.y > 2*game.height/3 && colton.y+colton.height < game.height-colton.width-15)
+        {
+            //  Move to the left
+            colton.body.velocity.y = 150;
+        }
     }
-    if ((cursors.up.isDown || game.input.pointer1.y < game.height/3 & game.input.pointer1.active) && colton.y > 65)
-    {
-        //  Move to the left
-        colton.body.velocity.y = -150;
-        //music.pause();
+    else{
+        if (cursors.left.isDown && colton.x > colton.width+15)
+        {
+            //  Move to the left
+            colton.body.velocity.x = -150;
+        }
+        else if (cursors.right.isDown && colton.x+colton.width < game.width-colton.width-15)
+        {
+            //  Move to the left
+            colton.body.velocity.x = 150;
+        }
+        if (cursors.up.isDown && colton.y > colton.width+15)
+        {
+            //  Move to the left
+            colton.body.velocity.y = -150;
+        }
+        else if (cursors.down.isDown && colton.y+colton.height < game.height-colton.width-15)
+        {
+            //  Move to the left
+            colton.body.velocity.y = 150;
+        }
     }
-    else if ((cursors.down.isDown || game.input.pointer1.y > 2*game.height/3 & game.input.pointer1.active) && colton.y+colton.height < game.height-50)
-    {
-        //  Move to the left
-        colton.body.velocity.y = 150;
-        //music.pause();
-    }
-
 }
